4466a89d4507f7d245ff80b268def7c1eaec97b5
chengfei <chengfei@iuitech.com>
2018-12-03 10:43:27 +0800

    add ehart compenonts

--- vue-element/src/views/dashboard/index.vue
+++ vue-element/src/views/dashboard/index.vue
@@ -2,14 +2,40 @@
   <div class="dashboard-container">
     <div class="dashboard-text">name:{{ name }}</div>
     <div class="dashboard-text">roles:<span v-for="role in roles" :key="role">{{ role }}</span></div>
+    <el-row :gutter="20">
+  <el-col :span="12" :offset="6">
+    <chart :option="option"/>
+  </el-col>
+    </el-row>
+    </el-row>
   </div>
 </template>
 
 <script>
 import { mapGetters } from 'vuex'
-
+import Chart from '@/components/Chart/index'
 export default {
   name: 'Dashboard',
+  components: {
+    Chart
+  },
+  data() {
+    return {
+      option: {
+        xAxis: {
+          type: 'category',
+          data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+        },
+        yAxis: {
+          type: 'value'
+        },
+        series: [{
+          data: [820, 932, 901, 934, 1290, 1330, 1320],
+          type: 'line'
+        }]
+      }
+    }
+  },
   computed: {
     ...mapGetters([
       'name',
