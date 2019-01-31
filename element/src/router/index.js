b33ce8a6a6b4dc8501d3b77751435fbd647fb9e8
BurNing1993 <786678257@qq.com>
2019-01-31 22:36:48 +0800

    up

---
+++ element/src/router/index.js
@@ -0,0 +1,30 @@
+import Vue from 'vue';
+import Router from 'vue-router';
+
+import Layout from '../views/layout/Layout.vue';
+
+Vue.use(Router);
+
+export const constantRouterMap = [
+  { path: '/login', component: () => import('@/views/login/index'), hidden: true },
+  { path: '/404', component: () => import('@/views/404'), hidden: true },
+
+  {
+    path: '/',
+    component: Layout,
+    redirect: '/dashboard',
+    name: 'Dashboard',
+    children: [{
+      path: 'dashboard',
+      component: () => import('@/views/dashboard/index'),
+      meta: { title: 'Dashboard', icon: 'link' },
+    }],
+  },
+  { path: '*', redirect: '/404', hidden: true },
+];
+
+export default new Router({
+  mode: 'history', // 后端支持可开
+  scrollBehavior: () => ({ y: 0 }),
+  routes: constantRouterMap,
+});
