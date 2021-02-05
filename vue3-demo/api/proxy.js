556144574474b56748d1bc682e01dc8c9e014eb3
Joey <786678257@qq.com>
2021-02-05 18:15:27 +0800

    chore:proxy

---
+++ vue3-demo/api/proxy.js
@@ -0,0 +1,15 @@
+const { createProxyMiddleware } = require('http-proxy-middleware')
+
+module.exports = (req, res) => {
+  // 创建代理对象并转发请求
+  createProxyMiddleware({
+    target:'http://112.126.67.212/',
+    changeOrigin: true,
+    pathRewrite: {
+      // 通过路径重写，去除请求路径中的 `/backend`
+      // 例如 /backend/user/login 将被转发到 http://backend-api.com/user/login
+      '^/api/': '/'
+    }
+  })(req, res)
+}
+
